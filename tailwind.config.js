eaecd74303cd6d0418b5982a413ac6ed4ec6a302
David L. Enock <118814205+davunju@users.noreply.github.com>
2024-03-31 15:06:58 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: '"Rubik", sans-serif',
+      },
+      screens: {
+        mobile: { max: "600px" },
+      },
+      colors: {
+        paleViolet: "hsl(276, 100%, 81%)",
+        moderateViolet: "hsl(276, 55%, 52%)",
+        desaturatedDarkViolet: "hsl(271, 15%, 43%)",
+        grayishBlue: "hsl(206, 6%, 79%)",
+        veryDarkDesaturatedViolet: "hsl(271, 36%, 24%)",
+        darkGrayishViolet: "hsl(270, 7%, 64%)",
+        lightGrayishViolet: "hsl(270, 20%, 96%)",
+        veryLightMagenta: "hsl(289, 100%, 72%)",
+        lightMagenta: "hsl(293, 100%, 63%)",
+        lightViolet: "hsl(264, 100%, 61%)",
+      },
+    },
+  },
+  plugins: [],
+};
